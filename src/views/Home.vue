836eb10be846eeb1f2d74e9d54af8e4ef2d80c57
xiaoshu <1324214367@qq.com>
2020-12-11 16:45:26 +0800

    初始化 aside 的 html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
             <h1>轮子UI</h1>
             <h2>一个厉害的UI框架</h2>
             <p class="actions">
-                <a href="">Github</a>
-                <a href="">开始</a>
+                <a href="https://github.com/">Github</a>
+                <router-link to='/doc'>开始</router-link>
              </p>
         </div>
     </div>
