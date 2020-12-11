692463df63b674e2f57c7a38b4b7bc3ea5abe789
xiaoshu <1324214367@qq.com>
2020-12-11 10:53:59 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Shu/>
+</template>
+
+<script>
+import Shu from './components/Shu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Shu:Shu
+  }
+}
+</script>
