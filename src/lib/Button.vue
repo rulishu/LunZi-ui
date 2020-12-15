d785d995112dbe8484009bdc70aa042d9ec396ee
xiaoshu <1324214367@qq.com>
2020-12-15 09:52:17 +0800

    添加 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,12 +1,19 @@
 <template>
-  <button>
+  <button class="lunzi-button" :class="`theme-${theme}`">
     <slot />
   </button>
 </template>
 
 <script lang='ts'>
 export default {
+  props:{
+    theme:{
+      type:String,
+      default: 'button'
+    }
+  }
 }
 </script>
+
 <style lang='scss' scoped>
 </style>
