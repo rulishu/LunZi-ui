2ae2c517039f4547fed712195222ad0f94cade45
xiaoshu <1324214367@qq.com>
2020-12-15 09:43:50 +0800

    创建 button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,6 +1,21 @@
 <template>
+  <div>Button 示例</div>
+  <h1>示例1</h1>
   <div>
-      Button文档
+    <Button >你好</Button>
   </div>
 </template>
 
+<script>
+import Button from '../lib/Button.vue'
+
+export default {
+  components:{Button},
+  setup() {
+    const onClick = ()=>{
+      console.log('hi')
+    }
+    return {onClick}
+  }
+}
+</script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,12 @@
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
+
+<script lang='ts'>
+export default {
+}
+</script>
+<style lang='scss' scoped>
+</style>
