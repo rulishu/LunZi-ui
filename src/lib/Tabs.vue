1e02b52a29142cb3fda253469327aaa57946e97c
xiaoshu <1324214367@qq.com>
2020-12-17 11:52:21 +0800

    遇到 Vue 3 bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,17 +1,31 @@
 <template>
-  <div>
-      <div v-for="(t,index) in titles" :key='index'>{{t}}</div>
-      <component v-for='(c,index) in defaults ' :is='c' :key="index"/>
+  <div class="lunzi-tabs">
+      <div class="lunzi-tabs-nav">
+        <div class="lunzi-tabs-nav-item" 
+            v-for="(t,index) in titles" 
+            @click="select(t)"
+            :class="{selected:t === selected}"
+            :key='index'>{{t}}</div>
+         </div>
+        <div class="lunzi-tabs-content"> 
+            <component class="lunzi-tabs-content-item" 
+             :is='current' />
+        </div>
   </div>
 </template>
 
 <script lang='ts'>
 import Tab from '../lib/Tab.vue'
+import {computed} from 'vue'
 
 export default {
+    props:{
+        selected:{
+            type:String 
+        }
+    },
     setup(props,context){
         const defaults = context.slots.default()
-        console.log(...defaults)
         defaults.forEach( (tag)=>{
             if(tag.type != Tab){
                 throw new Error('Tabs 子标签必须是Tab')
@@ -20,7 +34,44 @@ export default {
         const titles = defaults.map((tag)=>{
             return tag.props.title
         })
-        return {defaults,titles}
+        const  current = computed(()=>{
+            console.log('重新 return')
+            return defaults.filter((tag)=>{
+            return tag.props.title === props.selected
+        })[0]
+        })
+        const select = (title: string)=>{
+            context.emit('update:selected',title)
+        }
+        return {defaults,titles,current,select}
     }
 }
 </script>
+
+<style lang="scss">
+    $blue: #40a9ff;
+    $color: #333;
+    $border-color: #d9d9d9;
+
+.lunzi-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
