9d7ea0265fa6dee7caeae671c4384ed9a72b0e64
xiaoshu <1324214367@qq.com>
2020-12-19 16:59:58 +0800

    支持markdown文件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -75,17 +75,15 @@
 import { ref } from 'vue'
 import Switch from '../lib/Switch.vue'
 import Button from '../lib/Button.vue'
-import Switch1Demo from './Switch1Demo.vue'
-import Switch2Demo from './Switch2Demo.vue'
 import 'prismjs'
 import 'prismjs/themes/prism.css'
 const Prism = (window as any).Prism
 
 export default {
-    components:{Switch,Button,Switch1Demo,Switch2Demo},
+    components:{Switch,Button,},
     setup() {
       const bool = ref(false)
-      return {bool,Switch1Demo,Switch2Demo,Prism}
+      return {bool,Prism}
     }
 }
 </script>
