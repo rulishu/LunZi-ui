eeb340468c90edd300027895ef7a613e7c1d311f
xiaoshu <1324214367@qq.com>
2020-12-21 12:34:27 +0800

    改造改造  Dialog demo

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,67 +1,23 @@
 <template>
- <h1>Dialog 示例</h1>
-    <div >
-        <div class="demo">
-            <h2>常规用法</h2>
-            <div class="demo-component">
-            <Button @click="toggle">toggle</Button>
-            <Dialog v-model:visible="x"                     :closeOnClickOverlay="false"
-            :ok="f1" :cancel="f2">
-
-            <template v-slot:title>
-                <strong>加粗的标题</strong>
-            </template>
-
-            <template v-slot:content>
-                <strong>内容</strong>
-                <div>文字</div>
-            </template>
-            </Dialog>
-            </div>
-        <div class="demo-actions">
-           <Button>查看代码</Button>
-        </div>
-        </div>
-    <div class="demo">
-        <h2>示例2</h2>
-        <div class="demo-component">
-        <Button @click="showDialog">show</Button>
-        </div>
-    <div class="demo-actions">
-          <Button>查看代码</Button>
-        </div>
-    </div>
-
-</div>
+ <div>Dialog 示例</div>
+    <Demo :component="Dialog1Demo" />
+    <Demo :component="Dialog2Demo" />
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue'
-import Button from '../lib/Button.vue'
-import { ref, h} from 'vue'
-import {openDialog} from '../lib/openDialog'
+import Demo from './Demo.vue';
+import Dialog1Demo from './Dialog1demo.vue';
+import Dialog2Demo from './Dialog2demo.vue';
 
 export default {
     components:{
-        Dialog,
-        Button
+        Demo
         },
     setup() {
-        const x = ref(false)
-        const toggle = ()=>{
-            x.value = !x.value
-        }
-        const f1 = ()=>{
-            return false
-        }
-        const f2 = ()=>{}
-        const showDialog = ()=>{
-            openDialog({
-                title: h('strong',{},'标题'),
-                content:'你好',
-            })
-        }
-        return {x,toggle,f1,f2,showDialog}
+      return {
+        Dialog1Demo,
+        Dialog2Demo
+       }
     }
 } 
 </script>
